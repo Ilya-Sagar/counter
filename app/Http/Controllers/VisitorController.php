5e6f8222d51b4308012d5766afa483ead07d1231
ilya-sagar <workspace144@gmail.com>
2021-10-04 21:16:10 +1200

    Create base methods structure of controller

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class VisitorController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request, int $id)
+    {
+        //
+    }
+
+    public function search(Request $request)
+    {
+        //
+    }
 }
