7cd5485c16b9e16f89504152ed4ac4bbd39223d6
ilya-sagar <workspace144@gmail.com>
2021-10-04 21:03:41 +1200

    Create routes for report controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', [\App\Http\Controllers\ReportCardController::class, 'index']);
+Route::post('reports', [\App\Http\Controllers\ReportCardController::class, 'create']);
+Route::put('reports/{id}', [\App\Http\Controllers\ReportCardController::class, 'update'])->where('id', '[0-9]+');
